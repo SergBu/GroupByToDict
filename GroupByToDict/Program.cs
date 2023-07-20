08773b48f642378062c414d13ce32b300fba6150
Sergei <sebudnikov@gmail.com>
2023-07-20 08:00:47 +0300

    Добавьте файлы проекта.

---
+++ GroupByToDict/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//тянем тс из бд
+using GroupByToDict;
+
+var allTerminalTimeslotVehiclesInTerminalByDate = new List<TerminalTimeslotVehicle>
+{
+
+};
+
+int[] allRestrictionsByTerminalAndDateIds = new int[3]{ 11, 12, 13 };
+
+var vehiclesInTerminalByDateWithoutQupta = (from a in allTerminalTimeslotVehiclesInTerminalByDate
+                                            where a.Deleted == null ? !allRestrictionsByTerminalAndDateIds.Intersect(a.RestrictionIds).Any() : !allRestrictionsByTerminalAndDateIds.Intersect(a.RestrictionsIdsInfo).Any()
+                                            select a).ToList();
+
+var allVehiclesGroupByCrop = vehiclesInTerminalByDateWithoutQupta
+    .GroupBy(x => new
+    {
+        x.CropId,
+        combination = x.CombinationId
+    }).ToDictionary(x => x.Key, x => x.ToList());
+
+foreach (var vehiclesWithCrop in allVehiclesGroupByCrop)
+{
+
+    var vehiclesWithoutQuota = new List<int>();
+
+    if (vehiclesWithoutQuota.Any())
+    {
+        var newVehiclesWithoutQuotasCropModelList = new
+        {
+            CropId = vehiclesWithCrop.Key.CropId ?? 0,
+            CombinationId = vehiclesWithCrop.Key.combination,
+            VehiclesWithoutQuota = vehiclesWithoutQuota
+        };
+    }
+}
---
+++ GroupByToDict/TerminalTimeslotVehicle.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+//using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GroupByToDict
+{
+    public class TerminalTimeslotVehicle
+    {
+        public TerminalTimeslotVehicle()
+        {
+            Version = 0;
+        }
+        [NotMapped]
+        public int TerminalTimeslotVehicleId { get; set; }
+
+        [NotMapped]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public int TerminalTimeslotId { get; set; }
+
+        [JsonProperty("ReservationId")]
+        public int TerminalTimeslotReservationId { get; set; }
+
+        public bool ShouldSerializeTerminalTimeslotReservationId() => TerminalTimeslotReservationId > 0;
+
+        /// <summary>
+        /// Версия
+        /// </summary>
+        [NotMapped]
+        [JsonIgnore]
+        public int Version { get; set; }
+
+        [JsonProperty("Restrictions")]
+        public IEnumerable<int> RestrictionIds { get; set; } = new List<int>();
+
+        [NotMapped]
+        public IEnumerable<int> RestrictionsIdsInfo { get; set; } = new List<int>();
+        public DateTime? Deleted { get; set; }
+        public virtual int? CropId { get; set; }
+        public virtual int CombinationId { get; set; }
+    }
+}
